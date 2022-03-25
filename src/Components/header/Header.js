563e3968d19d6e6f649456f884d27773cfbc56c8
muyed moktadir <muyed004@gmail.com>
2022-03-26 00:08:14 +0600

    header added

---
+++ src/Components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <h1>The Cozy Library</h1>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
